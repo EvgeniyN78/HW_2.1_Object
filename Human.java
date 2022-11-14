a5da919e1b27bfb5cc05e28e99c49bd3943618a5
Denis <9150771050@mail.ru>
2022-11-14 22:25:32 +0300

    HW_2.1_Object Задача№1

--- Human.java
+++ Human.java
@@ -1,4 +1,39 @@
 public class Human {
 
+    public int yearOfBirth;
+    public String name;
+    public String town;
+    public String job;
 
+    public Human(int yearOfBirth, String name, String town, String job) {
+        if (yearOfBirth < 0) {
+            yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+
+        if (name == null || name.equals("")) {
+            this.name = "\"Информация не указана\"";
+        } else {
+            this.name = name;
+        }
+
+        if (town == null || town.equals("")) {
+            this.town = "\"Информация не указана\"";
+        } else {
+            this.town = town;
+        }
+
+        if (job == null || job.equals("")) {
+            this.job = "\"Информация не указана\"";
+        } else {
+            this.job = job;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!";
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задание 1.\n");
+        Human Maxim = new Human(-1988, null, "", "");
+        Human Anna = new Human(1993, "Аня", "Москва", "Методиста образовательных программ");
+        Human Katya = new Human(1992, "Катя", "Калиниград", "Продакт менеджера");
+        Human Artem = new Human(1995, "Артём", "Москва", "Директора по развитию бизнеса");
+
+        System.out.println(Maxim.toString());
+        System.out.println(Anna.toString());
+        System.out.println(Katya.toString());
+        System.out.println(Artem.toString());
+
     }
 }
